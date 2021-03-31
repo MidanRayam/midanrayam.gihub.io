8791b138cb664b60f27bff08b377c03639475a90
Nadim Mayar <8792064+MidanRayam@users.noreply.github.com>
2021-03-31 10:45:07 +0200

    Update hello_script.js

--- hello_script.js
+++ hello_script.js
@@ -1,11 +1,4 @@
-var ready;
 
-ready = function () {
-   setTimeout(function(){ 
-     document.getElementById("hello_complex").innerHTML = "Hello World";
-   }, 3000);
-};
-
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
+setTimeout(function(){ 
+  document.getElementById("hello_complex").innerHTML = "Hello World";
+}, 3000);
