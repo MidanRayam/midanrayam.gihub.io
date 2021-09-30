467daaca0d42505e405aaee5cf8abe3d006ec0e1
Nadim Mayar <8792064+MidanRayam@users.noreply.github.com>
2021-09-30 11:12:24 +0200

    Update hello_script.js

--- hello_script.js
+++ hello_script.js
@@ -3,3 +3,11 @@ setTimeout(function(){
   console.log("==================");
   document.getElementById("hello_complex").innerHTML = "Hello Worldds";
 }, 3000);
+
+
+var calculate = function() {
+  var total_price = document.getElementById('textbox_id').value;
+  var total_shipment = document.getElementById('total_ship').value; 
+  var product_price = document.getElementById('product_price').value; 
+  document.getElementById('result').innerHTML = product_price;
+}
