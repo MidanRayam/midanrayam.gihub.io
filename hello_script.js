da4ad252feb054aa26dd81ec3c1c8ec1c456ad3b
Nadim Mayar <8792064+MidanRayam@users.noreply.github.com>
2021-09-30 11:37:03 +0200

    Update hello_script.js

--- hello_script.js
+++ hello_script.js
@@ -12,6 +12,6 @@ var calculate = function() {
   var shipment_rate = total_shipment/total_price;
   var shipment_cost = product_price*shipment_rate;
   var product_price_with_shiment = product_price + shipment_cost;
-  document.getElementById('result').innerHTML = "Shipment cost: "+ Math.round(shipment_cost * 100) / 100
- + " \nProduct price including shipment: " + Math.round(product_price_with_shiment * 100) / 100;
+  document.getElementById('shipment_cost').innerHTML = Math.round(shipment_cost * 100) / 100;
+  document.getElementById('total_product_pice').innerHTML = Math.round(product_price_with_shiment * 100) / 100;
 }
