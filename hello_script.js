bfde38c36ead70b03cbc81639b2c3c00c4cda448
Nadim Mayar <8792064+MidanRayam@users.noreply.github.com>
2021-09-30 11:39:23 +0200

    Update hello_script.js

--- hello_script.js
+++ hello_script.js
@@ -6,9 +6,9 @@
 
 
 var calculate = function() {
-  var total_price = document.getElementById('total_price').value;
-  var total_shipment = document.getElementById('total_ship').value; 
-  var product_price = document.getElementById('product_price').value; 
+  var total_price = parseFloat(document.getElementById('total_price').value);
+  var total_shipment = parseFloat(document.getElementById('total_ship').value); 
+  var product_price = parseFloat(document.getElementById('product_price').value); 
   var shipment_rate = total_shipment/total_price;
   var shipment_cost = product_price*shipment_rate;
   var product_price_with_shiment = product_price + shipment_cost;
